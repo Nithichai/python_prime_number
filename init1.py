b32b95d4f7b894ee0808e3766e8686c9d1fa1d6e
NopyJF <Nithichai@users.noreply.github.com>
2018-02-07 19:39:13 +0700

    Delete File

--- init1.py
+++ init1.py
@@ -3,12 +3,21 @@ import datetime
 import math
 
 n = input("Max value : ")
-start_t = time.time()
-A = [True] * (n+1)
-for i in range(2, int(math.sqrt(n))+1):
-	if A[i] is True:
-		for j in range(i*i, n+1, i):
-			A[j] = False
+round = input("Round : ")
+ls_time = []
 
-print(time.time() - start_t)
-# print([i for i in range(len(A)) if A[i] and i > 1])
+def sieve_time(n, round):
+	start_t = time.time()
+	A = [True] * (n+1)
+	for i in range(2, int(math.sqrt(n))+1):
+		if A[i] is True:
+			for j in range(i*i, n+1, i):
+				A[j] = False
+	return (time.time() - start_t) * 1000000
+
+for i in range(round):
+	ls_time.append(sieve_time(n, round))
+
+print("Min of time : %f us." % min(ls_time))
+print("Max of time : %f us." % max(ls_time))
+print("Avg of time : %f us." % (max(ls_time) / len(ls_time)))
